b20ecdddea74cb882771cee7bf52a55ccbc7902b
WyattStanley <rws24@uw.edu>
2023-11-08 14:37:23 -0800

    new file:   css/main.css 	new file:   index.html 	new file:   js/main.js 	.DS_Store 	section1.html 	section2.html 	section3.html

---
+++ js/main.js
@@ -0,0 +1,71 @@
+// Get the element with the class "icon"
+let icon = document.getElementsByClassName("icon")[0];
+
+// Add an event listener for the 'click' event on the icon element
+icon.addEventListener('click', responsive_control);
+
+// Function to control the responsiveness of the navigation bar
+function responsive_control() {
+  // Get the element with the id "myTopnav"
+  let x = document.getElementById("myTopnav");
+
+  // Check if the class name of the element is "topnav"
+  if (x.className === "topnav") {
+    // If it is, add the "responsive" class to the element
+    x.className += " responsive";
+  } else {
+    // If it's not, remove the "responsive" class from the element
+    x.className = "topnav";
+  }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the images and insert them inside the modal - use their "alt" text as a caption
+var images = document.querySelectorAll("[id^='myImg']");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Function to open the modal for a specific image
+function openModal(imageElement) {
+  modal.style.display = "block";
+  modalImg.src = imageElement.src;
+  captionText.innerHTML = imageElement.alt;
+}
+
+// Add click event listeners to each image
+images.forEach(function(image) {
+  image.onclick = function() {
+    openModal(this);
+  };
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+
